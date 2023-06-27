2b015e8b633cb1988547af6a0c62ffdfbaba58ae
Correa7 <correadamian2019@gmail.com>
2023-06-27 13:30:49 -0300

    Login logout register views footer profile

--- src/routes/sessions/sessions.route.js
+++ src/routes/sessions/sessions.route.js
@@ -6,6 +6,33 @@ const Service = new UserServices()
 const {Router} = express
 const router = new Router()
 
+async function auth (req, res, next){
+    let {email,password}= req.body
+    if (req.session.userEmail){
+        next()
+    }
+    return res.status(401).send('Error en Autenticacion')
+    // console.log(email,' :', password)
+    // try{
+    //     let users= await Service.getAll()
+    //     let userFound= users.find(user=>{
+    //        return user.email == email && user.password == password
+    //     } )
+    //     if(userFound){
+    //       next()
+    //     }
+    //     return res.status(401).send('Error en Autenticacion')
+
+    //     }
+    //     catch{
+    //         return res.status(500).json({
+    //             status: 'error',
+    //             msg: 'something went wrong :(',
+    //             data: {},
+    //         });
+    //     }
+}
+
 router.get('/register', (req,res)=>{
     res.status(200).render('register',{
         style: 'register.css',
@@ -19,6 +46,8 @@ router.get('/login', (req,res)=>{
     })
 })
 router.post('/login', async (req,res)=>{
+    let session=req.session.userEmail
+    console.log(session)
     let {email,password}= req.body
     try{
         let users= await Service.getAll()
@@ -29,7 +58,11 @@ router.post('/login', async (req,res)=>{
             console.log('Usuario Encontrado', userFound)
             req.session.userEmail= email
             req.session.userPassword= password
-            res.redirect('/session/profile')
+            
+            res.render('profile',{
+                data:{name:userFound.first_name,last_name:userFound.last_name},
+                // session:session
+            })
         }
         else{
             console.log('Usuario no encontrado')
@@ -48,13 +81,15 @@ router.post('/login', async (req,res)=>{
 router.get('/profile', (req,res)=>{
    res.render('profile',{
     style:'profile.css',
-    title:'Profile'
+    title:'Profile',
+    data:data,
+  
    })
 })
 router.get('/logout', (req,res)=>{
     req.session.destroy(err=>{
         if(err) res.send('Failed Logout')
-        res.redirect('/session/register')
+        res.redirect('/session/login')
     })
 })
 
